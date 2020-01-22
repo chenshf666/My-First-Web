acd9350ca7eeb368db173a09c54850e96cf2f7d7
chenshf <1795176826@qq.com>
2020-01-22 13:11:15 +0800

    完成了一个简单的ui

---
+++ move/star.js
@@ -0,0 +1,10 @@
+var body = document.getElementsByTagName('body')[0];
+for (var i = 0; i <= 0; i++) {
+	var star = document.createElement('div');
+	star.style.width = '3px';
+	star.style.height = '3px';
+	star.style.backgroundColor = 'white';
+	star.style.animation = 'starMove 10s';
+	body.appendChild(star);
+}
+
